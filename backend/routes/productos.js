51e80295e8f107771b5e847e0818f119b77ab6f1
joacojachow <joacojachow@hotmail.com>
2025-06-20 15:25:21 -0300

    new products routes added

--- backend/routes/productos.js
+++ backend/routes/productos.js
@@ -1,42 +1,35 @@
 const express = require('express');
 const router = express.Router();
-let productos = require('../data/productos.json');
+const Producto = require('../models/Producto');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const { categoria } = req.query;
-
-  if (categoria) {
-    const filtrados = productos.filter(
-      p => p.categoria.toLowerCase() === categoria.toLowerCase()
-    );
-    return res.json(filtrados);
-  }
-
+  const query = categoria ? { categoria: new RegExp(`^${categoria}$`, 'i') } : {};
+  const productos = await Producto.find(query);
   res.json(productos);
 });
 
-router.get('/:id', (req, res) => {
-  const producto = productos.find(p => p.id === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+  const producto = await Producto.find(p => p.id === parseInt(req.params.id));
   if (!producto) return res.status(404).json({ mensaje: 'Producto no encontrado' });
   res.json(producto);
 });
 
-router.post('/', (req, res) => {
-  const nuevo = { id: productos.length + 1, ...req.body };
-  productos.push(nuevo);
+router.post('/', async (req, res) => {
+  const nuevo = new Producto(req.body);
+  await nuevo.save();
   res.status(201).json(nuevo);
 });
 
-router.post('/disponibles', (req, res) => {
-  const disponibles = productos.filter(p => p.disponible === true);
+router.post('/disponibles', async (req, res) => {
+  const disponibles = Producto.filter(p => p.disponible === true);
   res.json(disponibles);
 });
 
-router.put('/:id', (req, res) => {
-  const index = productos.findIndex(p => p.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ mensaje: 'Producto no encontrado' });
-  productos[index] = { ...productos[index], ...req.body };
-  res.json(productos[index]);
+router.put('/:id', async  (req, res) => {
+  const actualizado = await Producto.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!actualizado) return res.status(404).json({ mensaje: 'Producto no encontrado' });
+  res.json(actualizado);
 });
 
 module.exports = router;
